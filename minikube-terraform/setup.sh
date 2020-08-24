4d413a84d994390d066af5c71726404401d830f4
rachelgreene <sheetal.prst@gmail.com>
2020-08-24 21:13:24 +0530

    Update setup.sh

--- minikube-terraform/setup.sh
+++ minikube-terraform/setup.sh
@@ -45,7 +45,9 @@ mv ./kubectl /usr/bin/kubectl
 kubectl version --short --client
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
 chmod +x ./minikube
-#mv ./minikube /usr/bin/minikube
+mv ./minikube /usr/bin/minikube
+cd /usr/bin
+yum install minikube -y
 minikube start --vm-driver=none || true
 yum install -y conntrack
 #minikube delete
