c5f657c792b1f23dedba06c18559b072603afacc
rachelgreene <sheetal.prst@gmail.com>
2020-08-31 22:02:49 +0530

    Update check_elasticsearch_data_flow.sh

--- elasticstack/test/check_elasticsearch_data_flow.sh
+++ elasticstack/test/check_elasticsearch_data_flow.sh
@@ -5,7 +5,9 @@ set -e
 FAILURE=1
 SUCCESS=0
 
-elasticsearch_cluster_ip="$(kubectl get svc elasticsearch --kubeconfig=config -o json | jq -r .items[0].spec.clusterIP)"
+#elasticsearch_cluster_ip="$(kubectl get svc elasticsearch --kubeconfig=config -o json | jq -r .items[0].spec.clusterIP)"
+elasticsearch_cluster_ip=kubectl get svc elasticsearch --kubeconfig=config| awk '{print $3}'|tail -1|awk '{print $1}'                                                                                                                    1
+
 elasticsearch_cluster_port="9200"
 
 random_index_name="$(cat /dev/urandom | tr -cd 'a-f0-9' | head -c6)"
