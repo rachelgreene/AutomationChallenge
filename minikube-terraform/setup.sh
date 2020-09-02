8692d7d448c4b8f6dc6bcf94dd61fac16c2a0e4d
rachelgreene <sheetal.prst@gmail.com>
2020-09-02 09:20:24 +0530

    Update setup.sh

--- minikube-terraform/setup.sh
+++ minikube-terraform/setup.sh
@@ -23,21 +23,6 @@ mkdir -p /mnt/data
 chmod -R 777 /usr/share/elasticsearch/data
 chmod -R 777 /mnt/data 
 
-# Setup gitlab-ci
-#docker volume create gitlab_data
-#external_public_ip="$(curl http://169.254.169.254/latest/meta-data/public-ipv4)"
-#echo "external_url 'http://$external_public_ip'" >> /tmp/gitlab.rb
-#chmod 777 /tmp/gitlab.rb
-#docker run -p 80:80 -p 443:443 -v gitlab_data:/var/opt/gitlab -v /tmp/gitlab.rb:/etc/gitlab/gitlab.rb -d gitlab/gitlab-ce
-#sleep 10
-
-# Install gitlab-runner
-#useradd --comment 'GitLab Runner' --create-home gitlab-runner --shell /bin/bash
-#curl -L --output /usr/bin/gitlab-runner https://gitlab-runner-downloads.s3.amazonaws.com/latest/binaries/gitlab-runner-linux-amd64
-#chmod +x /usr/bin/gitlab-runner
-#gitlab-runner install --working-directory /home/gitlab-runner --user root
-#gitlab-runner start
-
 # Install minikube and kubectl
 curl -o kubectl https://amazon-eks.s3-us-west-2.amazonaws.com/1.13.7/2019-06-11/bin/linux/amd64/kubectl
 chmod +x ./kubectl
